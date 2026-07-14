902df31bd0663fd7239568bc4060aabf8920342f
MrPuzzles <149252480+NJded@users.noreply.github.com>
2026-07-14 17:07:23 +0700

    Update CounsellorMutagen.cs

--- Content/Items/Accessories/CounsellorMutagen.cs
+++ Content/Items/Accessories/CounsellorMutagen.cs
@@ -25,19 +25,23 @@ namespace RagnarokOfRedemptionAPI.Content.Items.Accessories
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
             player.GetDamage(DamageClass.Summon) += 0.25f;
-            player.GetCritChance(DamageClass.Summon) += 0.15f;
+
+            player.whipRangeMultiplier *= 1.15f;
+
+            var modPlayer = player.GetModPlayer<CounsellorMutagenPlayer>();
+            modPlayer.minionCritChance = 0.07f;
         }
 
         public override void AddRecipes()
         {
             Mod redemption = ModLoader.GetMod("Redemption");
             Mod continent = ModLoader.GetMod("ContinentOfJourney");
-            Mod homeward = ModLoader.GetMod("HomewardRagnarok");
+            Mod calamity = ModLoader.GetMod("CalamityMod");
 
             int mutagenType = redemption.Find<ModItem>("MutagenSummon").Type;
             int badgeType = continent.Find<ModItem>("CounsellorBadge").Type;
             int crownType = continent.Find<ModItem>("WillToCrown").Type;
-            int tileType = homeward.Find<ModTile>("TimelessFountainTile").Type;
+            int tileType = calamity.Find<ModTile>("CosmicAnvil").Type;
 
             CreateRecipe()
                 .AddIngredient(mutagenType)
@@ -48,3 +52,24 @@ namespace RagnarokOfRedemptionAPI.Content.Items.Accessories
         }
     }
 }
+
+    public class CounsellorMutagenPlayer : ModPlayer
+    {
+        public float minionCritChance = 0f;
+
+        public override void ResetEffects()
+        {
+            minionCritChance = 0f;
+        }
+
+        public override void ModifyHitNPCWithProj(Projectile proj, NPC target, ref NPC.HitModifiers modifiers)
+        {
+            if (proj.DamageType == DamageClass.Summon && minionCritChance > 0f)
+            {
+                if (Main.rand.NextFloat() < minionCritChance)
+                {
+                    modifiers.SetCrit();
+                }
+            }
+        }
+    }
